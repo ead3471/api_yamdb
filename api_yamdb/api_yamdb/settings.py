d690380a8c2d878e3bd99c2c43315a072981c2e2
Andrey Kovchegin <akovchegin@gmail.com>
2023-01-03 02:15:07 +0300

    Added custom permissions

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -106,3 +106,11 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 # Auth customization
 
 AUTH_USER_MODEL = 'users.User'
+
+# DRF
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
